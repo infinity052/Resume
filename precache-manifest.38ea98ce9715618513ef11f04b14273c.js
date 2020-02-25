82a438ec96295bcbe2241cbb09cd4e54f00cee43
infinity052 <28987176+infinity052@users.noreply.github.com>
2020-02-25 19:08:04 +0530

    Updates

---
+++ precache-manifest.38ea98ce9715618513ef11f04b14273c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cd4b9769ade971140ae1b14d3cbedb",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "c41fc2c64a8c231df5be",
+    "url": "/Resume/static/css/main.ff8d3a74.chunk.css"
+  },
+  {
+    "revision": "561dbb877b821658f957",
+    "url": "/Resume/static/js/2.6197e445.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Resume/static/js/2.6197e445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41fc2c64a8c231df5be",
+    "url": "/Resume/static/js/main.0efb088e.chunk.js"
+  },
+  {
+    "revision": "a8bd4d37f56736925879",
+    "url": "/Resume/static/js/runtime-main.04142021.js"
+  },
+  {
+    "revision": "3a677c94ce87131865cd27365b889e7d",
+    "url": "/Resume/static/media/IMG_20200214_160918_841.3a677c94.jpg"
+  },
+  {
+    "revision": "13e2f94bfd3e499dcc649e1e055a9391",
+    "url": "/Resume/static/media/about.13e2f94b.jpeg"
+  },
+  {
+    "revision": "0d49e8f9703163d387be8417ca36a289",
+    "url": "/Resume/static/media/bill.0d49e8f9.jpg"
+  },
+  {
+    "revision": "7de15fa6014f61eaf88693a084f90772",
+    "url": "/Resume/static/media/card-back.7de15fa6.jpg"
+  },
+  {
+    "revision": "a62a7063882763aec31c19c2098b71a9",
+    "url": "/Resume/static/media/classmarker.a62a7063.jpg"
+  },
+  {
+    "revision": "85cd18f0a587e0aa3d68de078bd9f9a8",
+    "url": "/Resume/static/media/education-background.85cd18f0.jpg"
+  },
+  {
+    "revision": "c610f9c17a152a817086473e172913a7",
+    "url": "/Resume/static/media/expense.c610f9c1.jpg"
+  },
+  {
+    "revision": "c6b208469e2a7d05cd3056581e04d0f8",
+    "url": "/Resume/static/media/experience.c6b20846.jpg"
+  },
+  {
+    "revision": "21f54cc16316ab760552d49a96ca11e4",
+    "url": "/Resume/static/media/planes.21f54cc1.jpg"
+  },
+  {
+    "revision": "f648443e3b74d61d448a81a0e2ccb502",
+    "url": "/Resume/static/media/sf2.f648443e.jpg"
+  },
+  {
+    "revision": "c02e3b5958f6fe108fa07c3e2b904678",
+    "url": "/Resume/static/media/tictactoe.c02e3b59.jpg"
+  },
+  {
+    "revision": "d41991a9398ebaefcc73c9bc7bafadee",
+    "url": "/Resume/static/media/todolist.d41991a9.jpg"
+  },
+  {
+    "revision": "a813c352157d5b5ecbbf4af9da7e1c1d",
+    "url": "/Resume/static/media/usercrudphp.a813c352.jpg"
+  }
+]);
